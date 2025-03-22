e69b3abf08566e2e051252362365df11b2594639
linikers <linikers@hotmail.com>
2025-03-22 11:45:58 -0300

    add colors

--- bitcommerce/src/app/components/product/produto.tsx
+++ bitcommerce/src/app/components/product/produto.tsx
@@ -1,30 +1,65 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { notFound } from "next/navigation";
 ///detalhes do produto especifico
-export default function ProdutoDetalhado({ params }: {params: {id: string}} ) {
-    const produtos = [
-        { id: 1, nome: 'Produto 1', preco: '100', img: '/images/produto1.jpg', descricao: 'teste teste teste' },
-        { id: 2, nome: 'Produto 2', preco: '200', img: '/images/produto2.jpg', descricao: 'teste teste teste' },
-      ];
-    const produto = produtos.find((p) => p.id ===Number(params.id));
+export default function ProdutoDetalhado({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const produtos = [
+    {
+      id: 1,
+      nome: "Produto 1",
+      preco: "100",
+      img: "/images/produto1.jpg",
+      descricao: "teste teste teste",
+    },
+    {
+      id: 2,
+      nome: "Produto 2",
+      preco: "200",
+      img: "/images/produto2.jpg",
+      descricao: "teste teste teste",
+    },
+  ];
+  const produto = produtos.find((p) => p.id === Number(params.id));
 
-    if (!produto) notFound();
+  if (!produto) notFound();
 
-    return (
-        <Container>
-            <Box my={4}>
-                <Typography variant="h3" component="h1" gutterBottom>{produto.nome}</Typography>
-            </Box>
-            <img src={produto.img} alt={produto.nome} style={{ maxWidth: '100%', height:'auto'}} />
-            <Typography variant="body1" paragraph>
-                {produto.descricao}
+  return (
+    <Container maxWidth="sm">
+      <Card sx={{ mt: 4, p: 2, borderRadius: 3, boxShadow: 3 }}>
+        <CardMedia
+          component="img"
+          image={produto.img}
+          alt={produto.nome}
+          sx={{ height: 240, objectFit: "cover", borderRadius: 2 }}
+        />
+        <CardContent>
+          <Stack spacing={2}>
+            <Typography variant="h4" fontWeight="bold">
+              {produto.nome}
             </Typography>
-            <Typography variant="h5" gutterBottom>
-                R$: {produto.preco}
+            <Typography variant="body1" color="text.secondary">
+              {produto.descricao}
             </Typography>
-            <Button variant="contained" color="primary">
-                Adicionar ao carrinho
+            <Typography variant="h5" fontWeight="bold" color="primary">
+              R$ {produto.preco}
+            </Typography>
+            <Button variant="contained" color="success" size="large">
+              Adicionar ao Carrinho
             </Button>
-        </Container>
-    )
+          </Stack>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 }
